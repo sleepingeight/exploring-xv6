5d3cc03aca1bb558f7d77e3dc5fd06de93cfd6dc
sleepingeight <theallrounder.mst@gmail.com>
2024-09-01 03:07:08 +0530

    initial commit

---
+++ xv6-proc-code/xv6/testcase.c
@@ -0,0 +1,41 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+void welcome(void)
+{
+  printf(1, "I am child in welcome function\n");
+  welcomeDone();
+}
+
+int main(int argc, char *argv[])
+{
+
+  int ret1 = fork();
+  if(ret1 == 0)
+    {
+      printf(1, "I am child with no welcome\n"); 
+    }
+  else if (ret1 > 0)
+    {
+      wait();
+      printf(1, "Parent reaped first child\n");
+     
+      int a = welcomeFunction(&welcome);
+      printf(1, "retval - %d", a);
+      
+      int ret2 = fork();
+      if (ret2 == 0)
+	{
+	  printf(1, "I have returned from welcome function\n");
+	  exit();
+	}
+      else if (ret2 > 0)
+	{
+	  wait();
+	  printf(1, "Parent reaped second child\n");
+	}
+    }
+  
+  exit();
+}
